c8b9c3056e3dc31a4b87a0fd17e74a0aeb50cef7
parksarang04 <qkrtkfkd9074@naver.com>
2024-09-24 08:23:00 +0900

    [Fix] 페키지 복구

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static int money;
+    
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class ShopManager : MonoBehaviour
 {
@@ -12,30 +13,44 @@ public class ShopManager : MonoBehaviour
     public GameObject productContent;
 
     public Button cartBtn;          //물품 장바구니 버튼 변수
-    public int money;               //게임 머니 변수
+    //public TMP_Text money;               //게임 머니 변수
     public TMP_Dropdown dropdown;   //물건 담는 드롭다운 버튼 변수
-    public Texture2D image;         //물건 이미지 변수 
-    public string productName;      //상품 물건 이름 변수
-    public int price;               //상품 물건 가격 변수
+    //public Image image;         //물건 이미지 변수 
+    //public TMP_Text productName;      //상품 물건 이름 변수
+    //public TMP_Text price;               //상품 물건 가격 변수
     public ProductData[] products;
-
     
+
     void Start()
     {
         Generateproduct();
+        products = Resources.LoadAll<ProductData>("");
     }
 
     public void Generateproduct()
     {
-        for(int i = 0; i < products.Length; i++)    //상품의 갯수만큼 상품창이 생긴다.
+        for (int i = 0; i < products.Length; i++)    //상품의 갯수만큼 상품창이 생긴다.
         {
             GameObject productObj = Instantiate(productPrefab, productContent.transform);     //Instantiate 복제라는 뜻
             //productObj.transform.SetParent(productContent.transform);
+            TextMeshProUGUI productName = productObj.transform.GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
+            Image productimage = productObj.transform.GetChild(1).GetComponentInChildren<Image>();
+            TextMeshProUGUI productprice = productObj.transform.GetChild(2).GetComponentInChildren<TextMeshProUGUI>();
+            if (productObj != null && products[i] != null)
+            {
+                productName.text = products[i] .name;
+                productimage = products[i].image;
+                productprice.text= products[i].buyCost.ToString(); //buyCost(int형),productprice(text형) 형변환ToString()
+            }
 
-            
+               
         }
 
 
+    }
+    public void OncartBtnClick(ProductData product)
+    {
+        
     }
 
 
